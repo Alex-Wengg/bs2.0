484fcfcdfaf3ebd85e4a2959e91db735034a2c0e
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-02-03 02:01:43 -0500

    add online-compiler

---
+++ online-compiler/index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const cors = require("cors");
+
+const { generateFile } = require("./generateFile");
+const { executeCpp } = require("./executeCpp");
+const { executePy } = require("./executePy");
+const fs = require('fs');
+
+const app = express();
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.post("/run", async (req, res) => {
+  const { language = "cpp", code } = req.body;
+
+  console.log(language, "Length:", code.length);
+
+  if (code === undefined) {
+    console.log(" code === undefined: ");
+
+    return res.status(400).json({ success: false, error: "Empty code body!" });
+  }
+  try {
+    // need to generate a c++ file with content from the request
+    const filepath = await generateFile(language, code);
+    // we need to run the file and send the response
+    let output;
+    if (language === "cpp") {
+      output = await executeCpp(filepath);
+    } else if (language === "py") {
+      output = await executePy(filepath);
+    }
+    fs.rmSync(filepath, {
+      force: true,
+  });
+    console.log(" output: ", output);
+    return res.json({ filepath, output });
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+app.listen(5000, () => {
+  console.log(`Listening on port 5000!`);
+});
