4a71d7681184826bd1cd211b27ceddb429ec3591
Han || Alex <36247722+Alex-Wengg@users.noreply.github.com>
2023-02-13 11:34:04 -0500

    Update socketConfig.js

--- client/src/socketConfig.js
+++ client/src/socketConfig.js
@@ -1,7 +1,7 @@
 import io from 'socket.io-client';
 //3001
 require("dotenv").config();
-const socket = io('https://coderace-ohjzsdclpa-uc.a.run.app');
-// const socket = io(process.env.REACT_APP_SOCKET);
+//const socket = io('https://coderace-ohjzsdclpa-uc.a.run.app');
+const socket = io(process.env.REACT_APP_SOCKET);
 
 export default socket;
